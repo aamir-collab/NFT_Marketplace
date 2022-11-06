45b5be3735dc7ee891b4f992a822756792bc8ee2
hikikomoriii <hikikomori377@gmail.com>
2022-11-06 14:20:27 +0530

    banner

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head';
 import Image from 'next/image';
 // import styles from '../styles/Home.module.css';
-
-export default function Home() {
+import { Banner } from '../components';
+const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline bg-nft-red-violet">hey </h1>
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          banner="Discover, collect, and sell extraordinary NFTs"
+          childStyles="md:text-4xl sm:text-2xl xs=text-xl text-left"
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />
+      </div>
     </div>
   );
-}
+};
+export default Home;
