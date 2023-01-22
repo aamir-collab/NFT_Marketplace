48f6075ee86ab5636a72ff60590163ac92565fb1
hikikomoriii <hikikomori377@gmail.com>
2023-01-22 13:55:38 +0530

    added

---
+++ components/CreatorCard.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// const CreatorCard = () => {
+//   return (
+//     <div>CreatorCard</div>
+//   )
+// }
+
+// export default CreatorCard
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+// import CreatorCard from '../components';
+// import CreatorCard from '../components/CreatorCard';
 // import styles from '../styles/Home.module.css';
-import { Banner } from '../components';
+import images from '../assets';
+import { Banner, CreatorCard } from '../components';
 const Home = () => {
+  const parentRef = useRef(null);
+  const scrollRef = useRef(null);
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -11,6 +17,26 @@ const Home = () => {
           childStyles="md:text-4xl sm:text-2xl xs=text-xl text-left"
           parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
         />
+        <div>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-8">
+            Best Creators
+          </h1>
+          {/* <div className="relative flex-1 max-w-full flex-mt-3" ref={parentRef}>
+            <div
+              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+              ref={scrollRef}
+            >
+              {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard
+                  key={`creator -${i}`}
+                  rank={i}
+                  creatorImage={images[`creator${i}`]}
+                  creatorName = {}
+                />
+              ))}
+            </div>
+          </div> */}
+        </div>
       </div>
     </div>
   );
